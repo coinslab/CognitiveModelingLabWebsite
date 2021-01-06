38ba2680cc429ca6ffcf6866db82ad024f6d679f
athulsudheesh <athuls0708@gmail.com>
2021-01-06 10:01:54 +0000

    Deploying to gh-pages from  @ 97bd3d366eda11397c4e7601d421381ec0a98e47 🚀

--- assets/model-zoo/code/ex1.jl
+++ assets/model-zoo/code/ex1.jl
@@ -2,8 +2,7 @@
 # Loading Packages & Models from packages
 using ScikitLearn
 using RDatasets
-using ScikitLearn.CrossValidation: cross_val_score
-@sk_import model_selection: train_test_split
+import ScikitLearn: CrossValidation
 @sk_import linear_model: LogisticRegression
 @sk_import metrics: confusion_matrix
 @sk_import metrics: classification_report
